4a4753bbc20bcf3ef9c5c14c7046ca9186d89b68
Jerry Chen <69334968+jerryvchen@users.noreply.github.com>
2023-09-08 22:45:16 -0700

    Edited app to show pdf preview

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 import sys
 
+from pathlib import PurePath
+from PySide6.QtCore import Qt
 from PySide6.QtGui import QImage, QPixmap
 from PySide6.QtWidgets import (QApplication, QMainWindow, QWidget, QHBoxLayout,
                                QVBoxLayout, QLabel, QFileDialog, QSpinBox, 
                                QGroupBox, QFormLayout, QLineEdit, 
                                QPlainTextEdit, QPushButton)
+
+from app_image import pdf_preview
 
 class PDF_Parser_App(QMainWindow):
     def __init__(self) -> None:
@@ -17,31 +21,48 @@ class PDF_Parser_App(QMainWindow):
 
         layout: QHBoxLayout = QHBoxLayout()
 
+        # Variables
+        self.file_path: PurePath | None = None
+
 
         ### COLUMN 1: PDF Selection
         col1_group: QGroupBox = QGroupBox("PDF Selection")
         col1_layout: QVBoxLayout = QVBoxLayout()
 
-        self.pdf_label: QLabel = QLabel("Preview Image")
-        col1_layout.addWidget(self.pdf_label)
-        self.test_button = QPushButton("test")
-        col1_layout.addWidget(self.test_button)
+        # Select PDF
+        self.file_path_title_label: QLabel = QLabel("Selected File: ", self)
+        col1_layout.addWidget(self.file_path_title_label)
+        self.file_path_label: QLabel = QLabel("No file selected")
+        col1_layout.addWidget(self.file_path_label)
+        self.select_pdf_button: QPushButton = QPushButton("Select PDF")
+        self.select_pdf_button.clicked.connect(self.select_pdf)
+        col1_layout.addWidget(self.select_pdf_button)
 
-        # self.select_pdf_button: QPushButton = QPushButton("Select PDF")
+        col1_layout.addStretch()
+
+        # PDF Preview
+        self.pdf_preview: QLabel = QLabel("Preview not Available")
+        self.pdf_preview.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        col1_layout.addWidget(self.pdf_preview)
+
+        col1_layout.addStretch()
+
 
         col1_group.setLayout(col1_layout)
+
         
 
 
-
         ### COLUMN 2: Fields and Pattern Matching
-        col2_group = QGroupBox("Fields and Pattern Matching")
+        col2_group: QGroupBox = QGroupBox("Fields and Pattern Matching")
         col2_layout: QVBoxLayout = QVBoxLayout()
 
         self.test_button2 = QPushButton("test2")
         col2_layout.addWidget(self.test_button2)
 
         col2_group.setLayout(col2_layout)
+
+
 
         ### COLUMN 3: Output Settings
         col3_group: QGroupBox = QGroupBox("Ouput Settings")
@@ -57,9 +78,15 @@ class PDF_Parser_App(QMainWindow):
         layout.addWidget(col3_group)
         main_widget.setLayout(layout)
     
-    def select_self(self):
-        options: QFileDialog = QFileDialog.options()
+    def select_pdf(self):
+        options: QFileDialog = QFileDialog.Options()    
         options |= QFileDialog.ReadOnly
+        pdf_file, _ = QFileDialog.getOpenFileName(self, "Select PDF File", "", "PDF files (*.pdf);;All Files (*)", options=options)
+        print(pdf_file)
+        if pdf_file:
+            self.file_path = PurePath(pdf_file)
+            self.file_path_label.setText(f"{self.file_path}")
+            pdf_preview(self.pdf_preview, self.file_path)
 
 
 if __name__ == "__main__":
--- app_image.py
+++ app_image.py
@@ -1,3 +1,4 @@
+from PySide6.QtCore import Qt, QSize
 from PySide6.QtGui import QImage, QPixmap
 from PySide6.QtWidgets import QLabel
 from pdf2image import convert_from_path
@@ -6,8 +7,17 @@ from pathlib import PurePath
 from PIL import Image
 
 
-def pdf_preview(file_path: PurePath, page: int = 1) -> QLabel:
-    preview_label: QLabel = QLabel()
+def pdf_preview(label: QLabel, file_path: PurePath, page: int = 1) -> None:
+    """
+    Converts a QLabel to show a picture instead. 
+
+    :param file_path: Path to the PDF.
+    :type file_path: pathlib.PurePath
+    :param page: The page which should be rendered, 1-indexed
+    :type: int
+    :return: A QLabel object containing the image
+    :rtype: PySide.QtWidgets.QLabel
+    """
 
     try:
         # Converts the first page of the PDF to an image
@@ -16,18 +26,18 @@ def pdf_preview(file_path: PurePath, page: int = 1) -> QLabel:
         # Convert the PIL image to a Qt-compatible QImage
         q_image: QImage = QImage(image.tobytes(), image.width, image.height, QImage.Format.Format_RGB888)
 
+        # Calculated the scaled size to fit the label while maintaining aspect ratio
+        label_width: QSize = label.size().width()
+        q_image = q_image.scaledToWidth(label_width)
+
         # Convert the QImage to a QPixmap
         pixmap: QPixmap = QPixmap.fromImage(q_image)
 
         # Add the QPixmap to the QLabel
-        preview_label.setPixmap(pixmap)
-        preview_label.setScaledContents(True) # Scale the image to fit the label
-    
+        label.setPixmap(pixmap)
+        label.setScaledContents(False)
+        label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
     except Exception as e:
         print(f"Error loading PDF: {e}")
-
-
-
-
-    return preview_label
 
